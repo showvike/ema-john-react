bbccb1f8ff6d7273c5e8bad6b2e80727076eccbb
showvike <showvike@gmail.com>
2023-04-05 15:56:27 +0600

    cart calculation done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,17 +3,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Cart.css";
 
 const Cart = ({ cart }) => {
-  const totalPrice = cart.reduce((prev, current) => prev + current.price, 0);
+  const orderSummary = cart.reduce(
+    (prev, next) => ({
+      totalPrice: prev.totalPrice + next.price,
+      totalShippingCharge: prev.totalShippingCharge + next.shipping,
+      tax: prev.tax + (next.price * 7) / 100,
+    }),
+    {
+      totalPrice: 0,
+      totalShippingCharge: 0,
+      tax: 0,
+    }
+  );
+  const { totalPrice, totalShippingCharge, tax } = orderSummary;
+  const grandTotal = totalPrice + totalShippingCharge + tax;
   return (
     <div className="cart">
       <h5>Order Summary</h5>
       <div>
         <p>Selected Items: {cart.length}</p>
         <p>Total Price: ${totalPrice}</p>
-        <p>Total Shipping Charge: $</p>
-        <p>Tax: $</p>
+        <p>Total Shipping Charge: ${totalShippingCharge}</p>
+        <p>Tax: ${tax.toFixed(2)}</p>
       </div>
-      <h6>Grand Total: $</h6>
+      <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
       <div className="buttons">
         <button>
           Clear Cart
