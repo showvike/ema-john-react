23f9a83aefa50ff2195fa3cc81a6b4cc9456b5c4
showvike <showvike@gmail.com>
2023-04-06 14:52:06 +0600

    display cart from local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -14,12 +14,16 @@ const Shop = () => {
   }, []);
   useEffect(() => {
     const storedCart = getShoppingCart();
+    const savedCart = [];
     for (const id in storedCart) {
       const addedProduct = products.find((product) => id === product.id);
-      const quantity = storedCart[id];
-      addedProduct.quantity = quantity;
-      console.log(addedProduct);
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        savedCart.push(addedProduct);
+      }
     }
+    setCart(savedCart);
   }, [products]);
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
