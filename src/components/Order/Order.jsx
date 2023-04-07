3953e582e94b535ff2a5182d5213a12ee2bd5ebf
showvike <showvike@gmail.com>
2023-04-08 02:56:35 +0600

    shared the cart

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -3,14 +3,14 @@ import { useLoaderData } from "react-router-dom";
 import Cart from "../Cart/Cart";
 
 const Order = () => {
-  const products = useLoaderData();
+  const cart = useLoaderData();
   return (
     <div className="shop">
       <div className="products">
-        <h1>This is order: {products.length}</h1>
+        <h1>This is order: {cart.length}</h1>
       </div>
       <div>
-        <Cart cart={[]}></Cart>
+        <Cart cart={cart}></Cart>
       </div>
     </div>
   );
--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,7 +1,19 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
 const cartProductsLoader = async () => {
   const loadedProducts = await fetch("./products.json");
   const products = await loadedProducts.json();
-  return products;
+  const storedCart = getShoppingCart();
+  const savedCart = [];
+  for (const id in storedCart) {
+    const addedProduct = products.find((pd) => id === pd.id);
+    if (addedProduct) {
+      const quantity = storedCart[id];
+      addedProduct.quantity = quantity;
+      savedCart.push(addedProduct);
+    }
+  }
+  return savedCart;
 };
 
 export default cartProductsLoader;
